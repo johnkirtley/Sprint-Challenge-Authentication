0eb6d2556fb5abebd3992df23b57032d37385df9
johnkirtley <kirtleyj16@gmail.com>
2020-03-27 09:11:07 -0700

    :sparkles: Added Login & Register endpoints
    Added login and register endpoints with authentication using jsonwebtoken

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,55 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const router = require('express').Router();
 
+const Users = require('./model');
+const { jwtSecret } = require('../config/secrets');
+
 router.post('/register', (req, res) => {
-  // implement registration
+	const userInfo = req.body;
+	const rounds = process.env.HASHING_ROUNDS || 10;
+	const hash = bcrypt.hashSync(userInfo.password, rounds);
+
+	userInfo.password = hash;
+
+	Users.add(userInfo)
+		.then(user => {
+			res.status(200).json(user);
+		})
+		.catch(err => {
+			console.log('Error adding user', err);
+		});
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+	const { username, password } = req.body;
+
+	Users.findBy({ username })
+		.then(([user]) => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				const token = generateToken(user);
+
+				res.status(201).json({ message: 'Logged In', token });
+			} else {
+				res.status(401).json({ message: 'Error logging in' });
+			}
+		})
+		.catch(err => {
+			console.log('Error logging in', err);
+		});
 });
+
+const generateToken = user => {
+	const payload = {
+		username: user.username,
+		password: user.password
+	};
+
+	const options = {
+		expiresIn: '1h'
+	};
+
+	return jwt.sign(payload, jwtSecret, options);
+};
 
 module.exports = router;
