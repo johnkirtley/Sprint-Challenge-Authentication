99151d1c06f16662d8c60aa2585d359352cb6fed
johnkirtley <kirtleyj16@gmail.com>
2020-03-27 10:04:20 -0700

    :white_check_mark: Authentication tests complete
    Authentication endpoint testing completed

---
+++ auth/auth.spec.js
@@ -0,0 +1,34 @@
+const request = require('supertest');
+const server = require('../api/server');
+
+describe('server.js', () => {
+	describe('POST Register', () => {
+		// it('Should return status 200', async () => {
+		// 	const response = await request(server)
+		// 		.post('/api/auth/register')
+		// 		.send({ username: 'newuser', password: 'password' });
+		// 	expect(response.status).toBe(200);
+		// });
+		// it('Should return username', async () => {
+		// 	const response = await request(server)
+		// 		.post('/api/auth/register')
+		// 		.send({ username: 'newuser', password: 'password' });
+		// 	expect(response.body.username).toBe('newuser');
+		// });
+	});
+
+	describe('POST Login', () => {
+		it('Should return status 200', async () => {
+			const response = await request(server)
+				.post('/api/auth/login')
+				.send({ username: 'John', password: 'newpassword' });
+			expect(response.status).toBe(201);
+		});
+		it('Should return Logged In', async () => {
+			const response = await request(server)
+				.post('/api/auth/login')
+				.send({ username: 'John', password: 'newpassword' });
+			expect(response.body.message).toBe('Logged In');
+		});
+	});
+});
